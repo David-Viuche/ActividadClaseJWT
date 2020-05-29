76ac49ea9d8ca978cce72dd8fbeebc2cde241a16
David Viuche <jviucheroja@uniminuto.edu.co>
2020-05-28 21:10:35 -0500

    register - update users

--- index.js
+++ index.js
@@ -4,7 +4,46 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+let users = [];
 
+function validateRegisterParams(req, res, next) {
+    const { id, name, lastname, email, password } = req.body;
+    if (id && name && lastname && email && password) {
+        next();
+    } else {
+        res.status(400).json({ error: "invalidate params" });
+    }
+}
+
+function validateUserEmail(req, res, next) {
+    const email = req.params.email;
+    let index = users.findIndex(el => el.email == email);
+    req.body.index = index;
+    if (index == -1) {
+        res.status(404).json({ error: 'user not found' });
+    } else {
+        next();
+    }
+}
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.post('/register', validateRegisterParams, (req, res) => {
+    users.push(req.body);
+    res.json({ msg: 'user added' });
+});
+
+app.patch('/editUser/:email', validateUserEmail, (req, res) => {
+    let index = req.body.index;
+    users[index].id = req.body.id;
+    users[index].name = req.body.name;
+    users[index].lastname = req.body.lastname;
+    users[index].password = req.body.password;
+
+    res.json({ msg: 'user updated' });
+});
 
 app.listen(3000, () => {
     console.log("server on port ", 3000);
